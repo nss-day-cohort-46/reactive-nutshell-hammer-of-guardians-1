087e603fafebafd53e9829627c70b9684b31dd34
ericberringer <eric.berringer@gmail.com>
2021-03-08 13:43:27 -0800

    Commented notes within each article module, with a breakdown of what to incorporate in each module.

---
+++ src/components/articles/ArticleCard.js
@@ -0,0 +1,14 @@
+/*Import react hooks, article context, article.css,
+useHistory and usePrams from react-router-dom 
+useContext Article Context {article delete function}
+useHistory()
+define articleDelete function call deletearticlefunction(article.id)
+.then push "/articles"
+maybe a useEffect goes here????
+
+Create ArticleCard function, pass in {article}.
+returns the html representation of the card.
+Use dot notation to access each article property.
+Include a delete button that is tied to each articles id.
+Our delete button will include an onClick which calls our articleDelete Function.
+ */
