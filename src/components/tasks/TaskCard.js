cbe2691f8aa325b98bfd4234432a812c74a05afd
nicoletatum <nicoletatum@outlook.com>
2021-03-11 11:37:56 -0600

    otw to getting checkbox to work

--- src/components/tasks/TaskCard.js
+++ src/components/tasks/TaskCard.js
@@ -1,14 +1,22 @@
 import React from "react"
 import "./Task.css"
 import { TaskContext } from "./TaskProvider"
+import { useHistory } from "react-router-dom"
 
 export const TaskCard = ({task}) => {
 
+    // const { deleteTask } = useContext(TaskContext)
+
+    const history = useHistory();
+    
     return (
         <section className="taskCard">
         <h3 className="taskName">{task.name}</h3>
         <div className="taskCompletionDate">Complete by: {task.date}</div>
-        {/* <input type="checkbox" className="taskCompleted"> </input> */}
+        <div>
+            Task completed  
+            <input type="checkbox" id="taskComplete" className="taskCompleted"/>
+        </div>
     </section>
     )
 }
--- src/components/tasks/TaskProvider.js
+++ src/components/tasks/TaskProvider.js
@@ -31,9 +31,21 @@ export const TaskProvider = (props) => {
         .then(getTasks)
     }
 
+    const updateTask = task => {
+        return fetch(`http://localhost:8088/tasks/${task.id}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(task)
+        })
+          .then(getTasks)
+      }
+
+
     return (
         <TaskContext.Provider value={{
-            tasks, getTasks, addTask, deleteTask
+            tasks, getTasks, addTask, deleteTask, updateTask
         }}>
             {props.children}
         </TaskContext.Provider>
