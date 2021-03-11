e8be6d6514d76ef478bf53c5f0ba001f2ecd3015
peytonkemp <peyton.kemp@gmail.com>
2021-03-11 11:53:18 -0600

    working on delete card

--- src/components/events/EventCard.js
+++ src/components/events/EventCard.js
@@ -1,14 +1,26 @@
-import React from "react"
+import React, {useContext} from "react"
+import { useHistory } from "react-router"
 import "./Event.css"
 import { EventContext } from "./EventProvider"
 
 export const EventCard = ({event}) => {
+
+    const { deleteEvent } = useContext(EventContext)
+    const history = useHistory()
+
+    const handleDelete = () => {
+        deleteEvent(event.id)
+        .then(() => {
+            history.push("/")
+        })
+    }
 
     return (
         <section className="eventCard">
             <h3 className="eventName">{event.name}</h3>
             <div className="eventLocation">{event.location}</div>
             <div className="eventDate">{event.date}</div>
+            <button onClick={handleDelete}>Remove Event</button>
         </section>
     )
 }
